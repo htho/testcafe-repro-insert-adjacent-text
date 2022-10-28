020284d569644cb11af4cfea9f29889fb6c3c806
Hauke Thorenz <hauke.thorenz@gti.de>
2022-10-28 10:05:19 +0200

    initial commit

---
+++ test.tc.ts
@@ -0,0 +1,14 @@
+import { fixture, Selector } from "testcafe";
+
+
+fixture("insertAdjacentText")
+	.page("http://localhost:3000/")
+
+test("insert text", async (t) => {
+    await t.click(Selector("#insert-text"));
+    await t.expect(Selector("#target").innerText).eql("1")
+});
+test("insert number", async (t) => {
+    await t.click(Selector("#insert-number"));
+    await t.expect(Selector("#target").innerText).eql("1")
+});
